04c34f4eb389e50ad37553110aa1addb4970bf7e
JeongWonYoung <nicednjsdud@naver.com>
2023-03-16 00:18:57 +0900

    test use static import qtype

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,7 @@ import study.querydsl.entity.Team;
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -53,12 +54,11 @@ public class QuerydslBasicTest {
 
     @Test
     public void startQuerydsl(){
-        QMember m = new QMember("m");
 
         Member findMember = jpaQueryFactory
-                .select(m)
-                .from(m)
-                .where(m.userName.eq("member1")) // 파라미터 바인딩 처리
+                .select(member)
+                .from(member)
+                .where(member.userName.eq("member1")) // 파라미터 바인딩 처리
                 .fetchOne();
 
         assertThat(findMember.getUserName()).isEqualTo("member1");
