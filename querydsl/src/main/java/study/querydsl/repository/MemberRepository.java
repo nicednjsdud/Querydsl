7a8bae06d8c7d9a7e0476e49a63b37bce7e26b49
JeongWonYoung <nicednjsdud@naver.com>
2023-05-12 00:34:10 +0900

    memberRepositorycustom 추가

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import study.querydsl.entity.Member;
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom{
 
     // select m from Member m where m.userName = ?
     List<Member> findByUserName(String userName);
