6922686aa1f25b9d26a44f7aecfad8414254e994
JeongWonYoung <nicednjsdud@naver.com>
2023-08-25 22:58:37 +0900

    쿼리 test

--- querydsl2/src/test/java/study/querydsl2/QurydslBasicTest.java
+++ querydsl2/src/test/java/study/querydsl2/QurydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl2;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,39 +14,42 @@ import study.querydsl2.entity.Team;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import static study.querydsl2.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
 public class QurydslBasicTest {
-    
+
     @Autowired
     EntityManager em;
 
+
     JPAQueryFactory queryFactory;
-    
+
     @BeforeEach
-    public void before(){
+    public void before() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
         em.persist(teamB);
 
-        Member member1 = new Member("member1",10,teamA);
-        Member member2 = new Member("member2",20,teamA);
-        Member member3 = new Member("member3",30,teamB);
-        Member member4 = new Member("member4",40,teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
 
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
         em.persist(member4);
-        
-        
+
+
     }
-    
+
     @Test
-    public void startJPQL(){
+    public void startJPQL() {
         // member1을 찾아라.
         Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", "member1")
@@ -54,7 +58,7 @@ public class QurydslBasicTest {
     }
 
     @Test
-    public void startQueryDsl(){
+    public void startQueryDsl() {
         queryFactory = new JPAQueryFactory(em);
         Member findMember = queryFactory
                 .select(member)
@@ -65,7 +69,8 @@ public class QurydslBasicTest {
     }
 
     @Test
-    public void search(){
+    public void search() {
+        queryFactory = new JPAQueryFactory(em);
         Member findMember = queryFactory
                 .selectFrom(member)
                 .where((member.username.eq("member1"))
@@ -75,7 +80,8 @@ public class QurydslBasicTest {
     }
 
     @Test
-    public void searchAndparam(){
+    public void searchAndparam() {
+        queryFactory = new JPAQueryFactory(em);
         Member findMember = queryFactory
                 .selectFrom(member)
                 .where(
@@ -83,5 +89,29 @@ public class QurydslBasicTest {
                         member.age.eq(10))
                 .fetchOne();
         Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+        queryFactory = new JPAQueryFactory(em);
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member findOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        results.getTotal();
+        List<Member> content = results.getResults();
+
     }
 }
