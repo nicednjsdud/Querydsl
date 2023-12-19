8c8b55fb0806f2636030007f752a001c93254016
JeongWonYoung <nicednjsdud@naver.com>
2023-12-19 22:57:12 +0900

    application.yml database 설정

--- querydsl3/src/test/java/bob/querydsl/QuerydslApplicationTests.java
+++ querydsl3/src/test/java/bob/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,21 @@
 package bob.querydsl;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
